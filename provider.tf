239c4c73a9b43454e5b821c73faa23633e72d31c
RokwonK <wonrok97@naver.com>
2023-07-10 00:07:30 +0900

    🎉 initial setting

---
+++ provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = "1.4.6"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region                   = "ap-northeast-2"
+  shared_config_files      = ["~/.aws/config"]
+  shared_credentials_files = ["~/.aws/credentials"]
+  profile                  = "keyme"
+}
---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "project_name" { }
+
+variable "host_domain_name" { }
+variable "sub_domain" { }
+variable "api_server_domain" { }
+# variable "ns_records" { }
+
+variable "vpc_cidr" { }
+variable "az_names" { }
+variable "public_subnets" { }
+variable "private_subnets" { }
+
+variable "ec2_inbound_rule" { }
+variable "db_inbound_rule" { }
+variable "outbound_rule" { }
+
+variable "key_pair_name" { }
+variable "db_username" { }
+variable "db_password" { }
