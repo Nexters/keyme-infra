4f1028405bc7502be76b8adb37bb37340a5593a5
RokwonK <wonrok97@naver.com>
2023-07-10 09:59:47 +0900

    ✨ ec2, ecr, rds, s3, api gateway output 추가

---
+++ outputs.tf
@@ -0,0 +1,35 @@
+output "ecr_repository_url" {
+  value = module.ecr.repository_url
+}
+
+output "ecr_repository_arn" {
+  value = module.ecr.arn
+}
+
+output "ec2_domain_name" {
+  value = aws_instance.public_ec2.public_dns
+}
+
+output "ec2_ip" {
+  value = aws_eip.ec2_eip.public_ip
+}
+
+output "rds_endpoint" {
+  value = aws_db_instance.db.endpoint
+}
+
+output "s3_domain" {
+  value = module.s3.domain_name
+}
+
+# output "ns_records" {
+#   value = aws_route53_zone.zone_main.name_servers
+# }
+
+output "api_gateway_endpoint" {
+  value = module.api_gateway.endpoint
+}
+
+output "api_gateway_url" {
+  value = module.api_gateway.url
+}
