d2ce2b2d66b475e1cabacbc2f6eaed5cccd9c290
RokwonK <wonrok97@naver.com>
2023-08-03 13:57:36 +0900

    S3 정책 내 public get 추가

--- modules/aws/s3/ec2_access/main.tf
+++ modules/aws/s3/ec2_access/main.tf
@@ -2,6 +2,15 @@ resource "aws_s3_bucket" "s3" {
   bucket = var.name
 }
 
+resource "aws_s3_bucket_public_access_block" "name" {
+  bucket = aws_s3_bucket.s3.id
+
+  block_public_acls = false
+  block_public_policy = false
+  ignore_public_acls = false
+  restrict_public_buckets = false
+}
+
 resource "aws_s3_bucket_website_configuration" "s3_hosting" {
   bucket = aws_s3_bucket.s3.id
 
@@ -21,6 +30,7 @@ resource "aws_s3_bucket_policy" "s3_policy" {
 
 data "aws_iam_policy_document" "s3_policy_data" {
   statement {
+    sid = "AllowAllS3ActionToEC2AndLambda"
     principals {
       type        = "Service"
       identifiers = [
@@ -32,6 +42,25 @@ data "aws_iam_policy_document" "s3_policy_data" {
     actions = [
       "s3:*",
     ]
+
+    effect = "Allow"
+
+    resources = [
+      "${aws_s3_bucket.s3.arn}/*",
+      "${aws_s3_bucket.s3.arn}",
+    ]
+  }
+
+  statement {
+    sid = "AllowGetPublic"
+    principals {
+      type = "AWS"
+      identifiers = [
+        "*"
+      ]
+    }
+
+    actions = [ "s3:GetObject" ]
 
     effect = "Allow"
 
